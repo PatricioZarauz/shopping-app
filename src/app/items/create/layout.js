4d338671eff0b27c5b974e32454d9d5eef05dfe9
PatricioZarauz <pwzarauz@gmail.com>
2023-12-03 21:04:44 -0300

    Added Title to items create page

--- src/app/items/create/layout.js
+++ src/app/items/create/layout.js
@@ -1,6 +1,7 @@
 const ItemsCreateLayout = ({ children }) => {
   return (
     <div className="mx-auto md:max-w-lg">
+      <h2 className="mx-auto text-4xl w-fit font-bold mt-2 mb-6">Create Item</h2>
       {children}
     </div>
   );
