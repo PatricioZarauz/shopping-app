da25360416c21f40a29dfd08cd7d125ee1489885
PatricioZarauz <pwzarauz@gmail.com>
2023-12-05 23:13:45 -0300

    Updated CategoryAccordion component

--- src/components/CategoryAccordion/index.js
+++ src/components/CategoryAccordion/index.js
@@ -1,87 +1,76 @@
 'use client'
 
-import { useEffect, useRef, useState } from "react";
+import { useState } from "react";
 import { RiDeleteBin6Line } from "react-icons/ri";
 import ItemCard from "../ItemCard";
+import { cn } from "@/lib/utils";
+import { textColorBasedOnBgColor } from "@/helpers";
+import { Draggable, Droppable } from "@hello-pangea/dnd";
+import { OneActionModal } from "../Modals";
 
-const aux = ['Ejemplo 1', 'Ejemplo 2', 'Ejemplo 3', 'Ejemplo 4'];
-
-const CategoryAccordion = () => {
-  const dragItem = useRef(null);
-  const dragOverItem = useRef(null);
-  const [items, setItems] = useState(aux);
-
-  const handleDragStart = (e, index) => {
-    // e.preventDefault();
-    // e.stopPropagation();
-    dragItem.current = index;
-    console.log('Start', index);
-  }
-  const handleDragEnter = (e, index) => {
-    // e.preventDefault();
-    // e.stopPropagation();
-    dragOverItem.current = index;
-    console.log('Enter', index);
-  }
-  const handleDragEnd = (e) => {
-    // e.preventDefault();
-    // e.stopPropagation();
-    console.log('End');
-    //duplicate items
-    let newItems = [...items];
-
-    //remove and save the dragged item content
-    const [draggedItemContent] = newItems.splice(dragItem.current, 1);
-
-    //switch the position
-    newItems.splice(dragOverItem.current, 0, draggedItemContent);
-
-    //reset the position ref
-    dragItem.current = null;
-    dragOverItem.current = null;
-
-    //update the actual array
-    setItems(newItems);
-  }
-
-  const handleDragOver = (e) => {
-    e.preventDefault();
-    // e.stopPropagation();
-    console.log('Over');
-  }
-
+const CategoryAccordion = ({ labelColor, name, displayFavDate = false, items }) => {
+  const [showModal, setShowModal] = useState(false);
+  const whiteText = '#FFFFFF' === textColorBasedOnBgColor(labelColor);
+  const textColor = {
+    "text-base-100": whiteText,
+    "text-primary-content": !whiteText
+  };
 
   return (
-    <div className="collapse collapse-arrow bg-base-200">
-      <input type="checkbox" />
-      <div className="collapse-title">
-        <div className="flex justify-between items-center">
-          <h3 className="text-xl font-medium flex-1">
-            Este es el Titulo
-          </h3>
-          <button className="btn btn-circle shadow-none shrink-0 z-10 text-red-600 text-2xl" onClick={() => console.log('Se Hizo Click')}>
-            <RiDeleteBin6Line />
-          </button>
-        </div>
-      </div>
-      <div className="collapse-content">
-        <ul>
-          {items.map((name, index) => (
-            <li
-              key={`${name}-${index}`}
-              className="my-1 rounded-md cursor-move"
-              draggable
-              onDragStart={(e) => handleDragStart(e, index)}
-              onDragEnter={(e) => handleDragEnter(e, index)}
-              onDragEnd={handleDragEnd}
-              onDragOver={handleDragOver}
+    <>
+      <div className={cn("collapse collapse-arrow max-w-2xl mx-auto", textColor)} style={{ backgroundColor: labelColor }}>
+        <input type="checkbox" />
+        <div className="collapse-title">
+          <div className="flex justify-between items-center">
+            <h3 className={cn("text-2xl font-bold flex-1", textColor)}>
+              {name}
+            </h3>
+            <button
+              className={cn("btn btn-circle border-none bg-inherit shadow-none shrink-0 z-10 text-xl",
+                {
+                  "text-base-100 hover:bg-base-100 hover:bg-opacity-20": whiteText,
+                  "text-primary-content hover:bg-primary-content hover:bg-opacity-20": !whiteText
+                })}
+              onClick={() => setShowModal(true)}
             >
-              <ItemCard name={name} />
-            </li>
-          ))}
-        </ul>
-      </div>
-    </div>
+              <RiDeleteBin6Line />
+            </button>
+          </div>
+        </div>
+        <div className="collapse-content">
+          <Droppable droppableId={name} type={name} isDropDisabled={false}>
+            {(provided) => (
+              <ul {...provided.droppableProps} ref={provided.innerRef}>
+                {items.map((item, index) => (
+                  <Draggable key={`${item.name}-${index}`} draggableId={item.name} index={index}>
+                    {(provided) => (
+                      <li
+                        className="my-1 cursor-grab active:cursor-grabbing rounded-md"
+                        {...provided.dragHandleProps}
+                        {...provided.draggableProps}
+                        ref={provided.innerRef}
+                      >
+                        <ItemCard {...item} displayFavDate={displayFavDate} />
+                      </li>
+                    )}
+                  </Draggable>
+                ))}
+                {provided.placeholder}
+              </ul>
+            )}
+          </Droppable>
+        </div>
+      </div >
+      {showModal && (
+        <OneActionModal
+          title="Delete Category"
+          mainActionText="Delete"
+          content={<p className="py-4">{`Are you sure that you want to delete "${name}"?`}</p>}
+          onCloseHandler={() => setShowModal(false)}
+          mainActionHandler={() => { console.log('Se Borro Category correctamente') }}
+        />
+      )}
+    </>
   );
 };
 
