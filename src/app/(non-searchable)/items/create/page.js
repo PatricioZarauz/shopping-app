be31589133fadf6b5b7085f88b248feb57673feb
PatricioZarauz <pwzarauz@gmail.com>
2023-12-06 03:18:05 -0300

    Added categories data integration with firebase

--- src/app/(non-searchable)/items/create/page.js
+++ src/app/(non-searchable)/items/create/page.js
@@ -1,7 +1,16 @@
 import ItemForm from "@/partials/ItemForm";
+import { collection, getDocs } from "firebase/firestore";
+import { db } from "@/firebase";
 
-const ItemsCreate = () => {
-  return (<ItemForm />);
+const ItemsCreate = async () => {
+  let categories = [];
+
+  const qs = await getDocs(collection(db, 'categories'));
+  qs.forEach((doc) => {
+    categories.push({ value: doc.id, label: doc.data().name })
+  });
+
+  return (<ItemForm categories={categories} />);
 };
 
 export default ItemsCreate;
