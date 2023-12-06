71e937df8c7072e09c53e7ffe266eda468e28ad6
PatricioZarauz <pwzarauz@gmail.com>
2023-12-06 03:16:55 -0300

    Added firebase integration to form submit

--- src/partials/CategoryForm/index.js
+++ src/partials/CategoryForm/index.js
@@ -5,21 +5,29 @@ import { yupResolver } from '@hookform/resolvers/yup';
 import { categorySchema } from "@/validations";
 import { TextInput, ColorInput } from "@/components/FormInput";
 import toast from "react-hot-toast";
+import { setDoc, doc } from "firebase/firestore";
+import { db } from "@/firebase";
+import hash from 'md5';
 
 const CategoryForm = () => {
-  const { handleSubmit, control } = useForm({
-    mode: 'onChange',
+  const { handleSubmit, control, reset } = useForm({
+    mode: 'onSubmit',
     defaultValues: {
-      name: 'Testing Category',
-      color: ''
+      labelColor: '#A3A3A3'
     },
     resolver: yupResolver(categorySchema)
   });
 
-  const onSubmit = (data) => {
-    console.log('data: ', data);
-    toast.success('Successfully created!');
-    toast.error('Error created!');
+  const onSubmit = async (data) => {
+    try {
+      const id = hash(data.name);
+      await setDoc(doc(db, "categories", id), data)
+      toast.success(`Successfully created "${data.name}" category!`);
+      reset();
+    } catch (e) {
+      toast.error('Sorry an error ocurred, please try again later');
+      console.error("Error adding category: ", e);
+    }
   };
 
   return (
