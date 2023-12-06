9459f453ed8655d78fa6ac858f7c516c9402a926
PatricioZarauz <pwzarauz@gmail.com>
2023-12-06 03:15:12 -0300

    Updated validations and added unique name validation

--- src/validations/categorySchema/index.js
+++ src/validations/categorySchema/index.js
@@ -1,6 +1,16 @@
 import * as yup from 'yup';
+import { doc, getDoc } from "firebase/firestore";
+import { db } from "@/firebase";
+import hash from 'md5';
+
+const validateUniqueCategoryName = async (name) => {
+  const id = hash(name);
+  const docSnap = await getDoc(doc(db, "categories", id));
+
+  return !docSnap.exists();
+}
 
 export default yup.object({
-  name: yup.string().required('The name is required'),
-  color: yup.string().trim().matches(/^#([0-9a-f]{3}|[0-9a-f]{6})$/i, 'The color must be a valid hex value').required('The color is required')
+  name: yup.string().required('The name is required').test('validateUniqueCategoryName', 'Name already being used, enter a new one', validateUniqueCategoryName),
+  labelColor: yup.string().trim().matches(/^#([0-9a-f]{3}|[0-9a-f]{6})$/i, 'The color must be a valid hex value').required('The color is required')
 }).required();
--- src/validations/itemSchema/index.js
+++ src/validations/itemSchema/index.js
@@ -1,7 +1,17 @@
 import * as yup from 'yup';
+import { doc, getDoc } from "firebase/firestore";
+import { db } from "@/firebase";
+import hash from 'md5';
+
+const validateUniqueItemName = async (name) => {
+  const id = hash(name);
+  const docSnap = await getDoc(doc(db, "items", id));
+
+  return !docSnap.exists();
+}
 
 export default yup.object({
-  name: yup.string().required('The name is required'),
-  category: yup.string().required('The category is required'),
-  image: yup.string().url('Please enter a valid URL').required('The image URL is required'),
+  name: yup.string().required('The name is required').test('validateUniqueItemName', 'Name already being used, enter a new one', validateUniqueItemName),
+  categoryId: yup.string().required('The category is required'),
+  image: yup.string().required('The image URL is required').url('Please enter a valid URL'),
 }).required();
