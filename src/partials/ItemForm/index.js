f3801d680e569f9e7fbdac90be7baebf63670ff3
PatricioZarauz <pwzarauz@gmail.com>
2023-12-06 03:16:44 -0300

    Added firebase integration to form submit

--- src/partials/ItemForm/index.js
+++ src/partials/ItemForm/index.js
@@ -5,31 +5,34 @@ import { yupResolver } from '@hookform/resolvers/yup';
 import { itemSchema } from "@/validations";
 import { AutoCompleteInput, TextInput } from "@/components/FormInput";
 import toast from "react-hot-toast";
+import { setDoc, doc } from "firebase/firestore";
+import { db } from "@/firebase";
+import hash from 'md5';
 
-const options = ['ejemplo', 'ejemplo 1', 'ejemplo 2', 'ejemplo 3', 'ejemplo 4', 'ejemplo 5', 'ejemplo 6', 'ejemplo 7', 'ejemplo 8', 'ejemplo 9', 'ejemplo 10', 'ejemplo 11', 'ejemplo 12', 'ejemplo 13', 'ejemplo 14']
-const parsedOptions = options.map((item) => ({ label: item, value: item }))
-
-const ItemForm = () => {
-  const { handleSubmit, control } = useForm({
-    mode: 'onChange',
-    defaultValues: {
-      image: 'https://i.natgeofe.com/k/1d33938b-3d02-4773-91e3-70b113c3b8c7/lion-male-roar.jpg?w=1084.125&h=609',
-      name: 'Testing',
-      category: 'ejemplo'
-    },
+const ItemForm = ({ categories }) => {
+  const { reset, handleSubmit, control } = useForm({
+    mode: 'onSubmit',
     resolver: yupResolver(itemSchema)
   });
 
-  const onSubmit = (data) => {
-    console.log('data: ', data);
-    toast.success('Successfully created!');
-    toast.error('Error created!');
+  const onSubmit = async (data) => {
+    const { categoryId, ...item } = data;
+    try {
+      const id = hash(item.name);
+      await setDoc(doc(db, "items", id), { name: item.name });
+      await setDoc(doc(db, "categories", categoryId, "items", id), item)
+      toast.success(`Successfully created "${item.name}" item!`);
+      reset();
+    } catch (e) {
+      toast.error('Sorry an error ocurred, please try again later');
+      console.error("Error adding item: ", e);
+    }
   };
 
   return (
     <form onSubmit={handleSubmit(onSubmit)} className="form-control">
       <TextInput label="Name" control={control} name="name" />
-      <AutoCompleteInput options={parsedOptions} label="Category" control={control} name="category" />
+      <AutoCompleteInput options={categories} label="Category" control={control} name="categoryId" />
       <TextInput label="Image URL" control={control} name="image" />
       <input className="btn btn-primary" type="submit" value="Create" />
     </form>
