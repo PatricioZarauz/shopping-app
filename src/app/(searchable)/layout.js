81a5ca6aa18af602c2d5bd276a154ba060d1539c
PatricioZarauz <pwzarauz@gmail.com>
2023-12-05 23:12:11 -0300

    Updated main page layout and rendered component

--- src/app/(searchable)/layout.js
+++ src/app/(searchable)/layout.js
@@ -3,7 +3,7 @@ import NavBar from "@/components/NavBar";
 const SearchableLayout = ({ children }) => {
   return (
     <NavBar hasSearchBar >
-      <main className="overflow-y-auto my-16 py-2 px-4">
+      <main className="overflow-y-auto mb-16 mt-32 py-2 px-4 md:my-16">
         {children}
       </main>
     </NavBar>
--- src/app/(searchable)/page.js
+++ src/app/(searchable)/page.js
@@ -1,21 +1,46 @@
-import { Fragment } from "react";
-import CategoryAccordion from "@/components/CategoryAccordion";
+import CategoriesList from "@/components/CategoriesList";
 
-const Home = () => {
-  const categories = [
-    { name: 'First Category', color: '#FAFAFA' },
-    { name: 'Second Category', color: '#FAFAFA' },
-    { name: 'Third Category', color: '#FAFAFA' },
-    { name: 'Forth Category', color: '#FAFAFA' },
-    { name: 'Fifth Category', color: '#FAFAFA' },
-    { name: 'Sixth Category', color: '#FAFAFA' }
+const Home = async () => {
+  const items = [
+    { name: 'Ejemplo 1', image: "https://daisyui.com/images/stock/photo-1635805737707-575885ab0820.jpg", favorite: false, favDate: null },
+    { name: 'Ejemplo 2', image: "https://daisyui.com/images/stock/photo-1635805737707-575885ab0820.jpg", favorite: true, favDate: "31/12/2023" },
+    { name: 'Ejemplo 3', image: "https://daisyui.com/images/stock/photo-1635805737707-575885ab0820.jpg", favorite: false, favDate: null },
   ];
+  const items1 = [
+    { name: 'Ejemplo 4', image: "https://daisyui.com/images/stock/photo-1635805737707-575885ab0820.jpg", favorite: false, favDate: null },
+    { name: 'Ejemplo 5', image: "https://daisyui.com/images/stock/photo-1635805737707-575885ab0820.jpg", favorite: true, favDate: "31/12/2023" },
+    { name: 'Ejemplo 6', image: "https://daisyui.com/images/stock/photo-1635805737707-575885ab0820.jpg", favorite: false, favDate: null },
+  ];
+  const items2 = [
+    { name: 'Ejemplo 7', image: "https://daisyui.com/images/stock/photo-1635805737707-575885ab0820.jpg", favorite: false, favDate: null },
+    { name: 'Ejemplo 8', image: "https://daisyui.com/images/stock/photo-1635805737707-575885ab0820.jpg", favorite: true, favDate: "31/12/2023" },
+    { name: 'Ejemplo 9', image: "https://daisyui.com/images/stock/photo-1635805737707-575885ab0820.jpg", favorite: false, favDate: null },
+  ];
+  const items3 = [
+    { name: 'Ejemplo 10', image: "https://daisyui.com/images/stock/photo-1635805737707-575885ab0820.jpg", favorite: false, favDate: null },
+    { name: 'Ejemplo 11', image: "https://daisyui.com/images/stock/photo-1635805737707-575885ab0820.jpg", favorite: true, favDate: "31/12/2023" },
+    { name: 'Ejemplo 12', image: "https://daisyui.com/images/stock/photo-1635805737707-575885ab0820.jpg", favorite: false, favDate: null },
+  ];
+  const items4 = [
+    { name: 'Ejemplo 13', image: "https://daisyui.com/images/stock/photo-1635805737707-575885ab0820.jpg", favorite: false, favDate: null },
+    { name: 'Ejemplo 14', image: "https://daisyui.com/images/stock/photo-1635805737707-575885ab0820.jpg", favorite: true, favDate: "31/12/2023" },
+    { name: 'Ejemplo 15', image: "https://daisyui.com/images/stock/photo-1635805737707-575885ab0820.jpg", favorite: false, favDate: null },
+  ];
+  const items5 = [
+    { name: 'Ejemplo 16', image: "https://daisyui.com/images/stock/photo-1635805737707-575885ab0820.jpg", favorite: false, favDate: null },
+    { name: 'Ejemplo 17', image: "https://daisyui.com/images/stock/photo-1635805737707-575885ab0820.jpg", favorite: true, favDate: "31/12/2023" },
+    { name: 'Ejemplo 18', image: "https://daisyui.com/images/stock/photo-1635805737707-575885ab0820.jpg", favorite: false, favDate: null },
+  ];
+  const categories = {
+    'First Category': { name: 'First Category', labelColor: '#ff6600', items },
+    'Second Category': { name: 'Second Category', labelColor: '#ffcc00', items: items1 },
+    'Third Category': { name: 'Third Category', labelColor: '#3366ff', items: items2 },
+    'Forth Category': { name: 'Forth Category', labelColor: '#00cc99', items: items3 },
+    'Fifth Category': { name: 'Fifth Category', labelColor: '#660033', items: items4 },
+    'Sixth Category': { name: 'Sixth Category', labelColor: '#0099cc', items: items5 }
+  };
 
-  return (
-    <Fragment>
-      <CategoryAccordion />
-    </Fragment>
-  );
+  return (<CategoriesList categories={categories} />);
 }
 
 export default Home;
