adf7ac7f244e54504b3004076264d926c74511de
PatricioZarauz <pwzarauz@gmail.com>
2023-12-07 13:35:13 -0300

    Added type and content prop

--- src/components/LoadingButton/index.js
+++ src/components/LoadingButton/index.js
@@ -1,17 +1,18 @@
 import { cn } from "@/lib/utils";
 
-const LoadingButton = ({ isLoading, styleProp, onClickAction, children }) => {
+const LoadingButton = ({ isLoading, styleProp, type = 'button', onClickAction, content }) => {
   return (
     <button
       className={cn("btn", styleProp)}
+      type={type}
       onClick={onClickAction}
     >
       {isLoading ? (
         <>
-          <span class="loading loading-spinner"></span>
+          <span className="loading loading-spinner"></span>
           {'loading'}
         </>
-      ) : { children }}
+      ) : (content)}
     </button>
   );
 };
